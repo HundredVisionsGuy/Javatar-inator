39b3c014c13def7a50a8fb987a4bdda7c54a5795
hundredvisionsguy <winikkc@hsd.k12.or.us>
2025-03-21 09:47:28 -0700

    adding GUI screen

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -2,8 +2,14 @@ package com.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 public class PrimaryController {
+    Button getImageButton = new Button();
+    TextField seedText = new TextField();
+    ImageView avatarImage = new ImageView();
 
     @FXML
     private void switchToSecondary() throws IOException {
